a67147aba6b351fd8b4cef4fbb6a3d40d53ce6e9
Marco Maizener <marco.maizener@gmail.com>
2019-05-11 11:26:56 +1000

    back to normal

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/marco/Documents/projects/microblog/venv"
+set -gx VIRTUAL_ENV "/Users/marco/Documents/projects/flask_exp/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
